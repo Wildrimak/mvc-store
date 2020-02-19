5172eff341b60451bfe1eb343d6f4ee757465693
= <=>
2020-02-19 19:12:35 -0300

    Modificado nome dos campos input e exibe mensagem de erro caso matricula ou senha estejam errados

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -10,13 +10,18 @@
         <?php echo form_open('login'); ?>
             <div align="center">
                 <div>
-                    <h5>Username</h5>
+                    <?php if( $this->session->flashdata('authError') )
+                        {
+                            echo $this->session->flashdata('authError');
+                        }
+                    ?>
+                    <h5>Matricula</h5>
                     <?php echo form_error('matricula'); ?>
                     <input type="text" name="matricula" value="<?php echo set_value('matricula'); ?>"/>    
                 </div>
 
                 <div>
-                    <h5>Password</h5>
+                    <h5>Senha</h5>
                     <?php echo form_error('senha'); ?>
                     <input type="password" name="senha" value="<?php echo set_value('senha'); ?>" />
                 </div>
