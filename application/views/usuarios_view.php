823c2a4e4f16ee268185a04e88d82e0cde688789
= <=>
2020-02-19 17:56:48 -0300

    Adicionado botão de logout e centralizado conteudo

--- application/views/usuarios_view.php
+++ application/views/usuarios_view.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Usuarios</title>
-</head>
-<body>
 
-    <table border="1">
-    <?php 
-        echo "<tr>
-                <th>id</th>
-                <th>nome</th>
-                <th>matricula</th>
-                <th>senha</th>
-                <th>created</th>
-                <th>status</th>
-            </tr>";
-        foreach ($usuarios as $usuario) {
-            echo "<tr>";
-            foreach ($usuario as $key => $value) {
-                echo "<td>".$value."</td>";
-            }
-            echo "</tr>";
-         }
-     ?>
-    </table>
+    <head>
+        <title>Usuarios</title>
+    </head>
 
-</body>
+    <body>
+
+        <div align="center">
+            <?php echo anchor('logout', 'Logout', 'title="logout" id="links"') ?>
+            <table border="1">
+            <?php 
+                echo "<tr>
+                        <th>id</th>
+                        <th>nome</th>
+                        <th>matricula</th>
+                        <th>senha</th>
+                        <th>created</th>
+                        <th>status</th>
+                    </tr>";
+                foreach ($usuarios as $usuario) {
+                    echo "<tr>";
+                    foreach ($usuario as $key => $value) {
+                        echo "<td>".$value."</td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+            </table>
+        </div>
+
+    </body>
+
 </html>
