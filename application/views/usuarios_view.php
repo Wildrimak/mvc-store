6299380424ba0010a1c54f9386227adbf06fea32
= <=>
2020-02-19 22:46:36 -0300

    Adicionado link para cada uma das 4 operações basicas

--- application/views/usuarios_view.php
+++ application/views/usuarios_view.php
@@ -11,23 +11,28 @@
             <?php echo anchor('logout', 'Logout', 'title="logout" id="links"') ?>
             <table border="1">
             <?php 
-                echo "<tr>
-                        <th>id</th>
-                        <th>nome</th>
-                        <th>matricula</th>
-                        <th>senha</th>
-                        <th>created</th>
-                        <th>status</th>
-                    </tr>";
+            echo "<tr>
+                    <th>id</th>
+                    <th>nome</th>
+                    <th>matricula</th>
+                    <th>senha</th>
+                    <th>created</th>
+                    <th>status</th>
+                    <th>edit</th>
+                    <th>delete</th>
+                </tr>";
                 foreach ($usuarios as $usuario) {
                     echo "<tr>";
                     foreach ($usuario as $key => $value) {
-                        echo "<td>".$value."</td>";
+                        echo "<td>".$value."</td>"; 
                     }
+                    echo "<td>". anchor('usuarios/editar/'.$usuario->id.'', 'Editar', 'title="editar" id="links"') ."</td>";
+                    echo "<td>". anchor('usuarios/deletar/'.$usuario->id.'', 'Deletar', 'title="deletar" id="links"')."</td>";
                     echo "</tr>";
                 }
             ?>
             </table>
+            <?php echo anchor('usuarios/adicionar', 'Adicionar Usuário', 'title="adicionar" id="links"') ?>
         </div>
 
     </body>
