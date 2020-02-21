071e9135c00e5a818e0eb4c89ebefc0a9141eee7
= <=>
2020-02-21 09:55:50 -0300

    Adicionado controller de vendas

---
+++ application/controllers/Venda_Controller.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Venda_Controller extends CI_Controller
+{
+    private $vendas;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('venda_model');
+        $this->load->model('usuario_model');
+        $this->load->model('produto_model');
+        $this->load->model('cliente_model');
+        
+        if (!$this->session->userdata('id')) {
+            redirect('login');
+        }
+
+        $this->vendas = $this->venda_model->fetch_all();
+    }
+
+    public function index()
+    {
+        $data["vendas"] = $this->vendas;
+        $this->load->view('vendas/vendas_view', $data);
+    }
+
+    public function adicionar()
+    {
+        $produtos = $this->produto_model->fetch_all();
+        $clientes = $this->cliente_model->fetch_all();
+        $data = array();
+        $data["produtos"] = $produtos;
+        $data["clientes"] = $clientes;
+        $this->load->view('vendas/adicionar_view', $data);
+    }
+
+    public function salvar_venda()
+    {
+        $produtos_id = $this->input->post("produtos_id");
+        $quantidade = $this->input->post("quantidade");
+        $forma_pagamento = $this->input->post("forma_pagamento");
+        $valor_total = $this->input->post("valor_total");
+        $data_cadastro = $this->input->post("data");
+        $clientes_id = $this->input->post("clientes_id");
+        $status = 1;
+        $session_id = $this->session->userdata('id');
+        $usuario_logado = $this->usuario_model->get($session_id)["id"];
+
+        $data = array(
+                "produtos_id" => $produtos_id,
+                "clientes_id" => $clientes_id,
+                "quantidade" => $quantidade,
+                "forma_pagamento" => $forma_pagamento,
+                "data" => $data_cadastro,
+                "valor_total" => $valor_total,
+                "usuarios_id" => $usuario_logado,
+                "status" => $status,
+            );
+        
+        $this->venda_model->insert($data);
+        
+        redirect('vendas/adicionar');
+    }
+}
