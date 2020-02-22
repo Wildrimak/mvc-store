72ac409056bdd04fe15de11620c14b3d231f41f2
wildrimak <=>
2020-02-21 23:55:14 -0300

    Melhorado apresentação visual de vendas

--- application/views/vendas/vendas_view.php
+++ application/views/vendas/vendas_view.php
@@ -1,28 +1,56 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="pt-br">
 
-    <head>
-        <title>Vendas</title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Vendas">
 
-    <body>
+    <title>Vendas</title>
 
-        <div align="center">
-            <?php echo anchor('logout', 'Logout', 'title="logout" id="links"') ?>
-            <table border="1">
+    <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.css');?>">
+</head>
+
+<body class="text-center">
+
+    <nav class="navbar navbar-dark justify-content-between bg-dark">
+
+        <ul class="nav">
+            <li class="nav-item">
+                <?php echo anchor('usuarios', 'Home', 'title="usuarios" class="nav-link active"') ?>
+            </li>
+            <li class="nav-item">
+                <?php echo anchor('clientes', 'Ver clientes', 'title="clientes" class="nav-link active"') ?>
+            </li>
+            <li class="nav-item">
+                <?php echo anchor('produtos', 'Ver produtos', 'title="produtos" class="nav-link active"') ?>
+            </li>
+        </ul>
+
+        <ul class="nav">
+            <li class="nav-item">
+                <?php echo anchor('logout', 'Logout', 'title="logout" class="nav-link active"') ?>
+            </li>
+        </ul>
+    </nav>
+
+
+    <div class="container">
+
+        <table class="table table-hover">
             <?php 
             echo "<tr>
-                    <th>id</th>
-                    <th>produtos_id</th>
-                    <th>clientes_id</th>
-                    <th>quantidade</th>
-                    <th>forma_pagamento</th>
-                    <th>data</th>
-                    <th>valor_total</th>
-                    <th>usuarios_id</th>
-                    <th>created</th>
-                    <th>updated</th>
-                    <th>status</th>
+                    <th>Id</th>
+                    <th>Produto</th>
+                    <th>Cliente</th>
+                    <th>Quantidade</th>
+                    <th>Forma de pagamento</th>
+                    <th>Data</th>
+                    <th>Valor Total</th>
+                    <th>Usuário</th>
+                    <th>Created</th>
+                    <th>Updated</th>
+                    <th>Status</th>
                 </tr>";
                 foreach ($vendas as $venda) {
                     echo "<tr>";
@@ -32,12 +60,12 @@
                     echo "</tr>";
                 }
             ?>
-            </table>
-            <?php echo anchor('vendas/adicionar', 'Realizar nova venda', 'title="adicionar" id="links"') ?>
-            <?php echo anchor('usuarios', 'Retornar', 'title="usuarios" id="links"') ?>
-    
-        </div>
+        </table>
+        <?php echo anchor('vendas/adicionar', 'Realizar nova venda', 'title="adicionar"') . "<br>" ?>
+        <?php echo anchor('usuarios', 'Retornar', 'title="usuarios" id="links"') ?>
 
-    </body>
+    </div>
+
+</body>
 
 </html>
