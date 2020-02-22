7f942df6294e57339ed106efb3540d5df66d3a0c
wildrimak <=>
2020-02-21 23:47:57 -0300

    Melhorado apresentação visual de clientes

--- application/views/clientes/clientes_view.php
+++ application/views/clientes/clientes_view.php
@@ -1,48 +1,77 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="pt-br">
 
-    <head>
-        <title>Clientes</title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Clientes">
 
-    <body>
+    <title>Clientes</title>
 
-        <div align="center">
-            <?php echo anchor('logout', 'Logout', 'title="logout" id="links"') ?>
-            <table border="1">
+    <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.css');?>">
+</head>
+
+<body class="text-center">
+
+    <nav class="navbar navbar-dark justify-content-between bg-dark">
+
+        <ul class="nav">
+            <li class="nav-item">
+                <?php echo anchor('usuarios', 'Home', 'title="usuarios" class="nav-link active"') ?>
+            </li>
+            <li class="nav-item">
+                <?php echo anchor('produtos', 'Ver produtos', 'title="produtos" class="nav-link active"') ?>
+            </li>
+            <li class="nav-item">
+                <?php echo anchor('vendas', 'Realizar Venda', 'title="vendas" class="nav-link active"') ?>
+            </li>
+        </ul>
+
+        <ul class="nav">
+            <li class="nav-item">
+                <?php echo anchor('logout', 'Logout', 'title="logout" class="nav-link active"') ?>
+            </li>
+        </ul>
+    </nav>
+
+
+    <div>
+
+        <table class="table table-hover">
             <?php 
             echo "<tr>
-                    <th>id</th>
-                    <th>nome</th>
-                    <th>cpf</th>
-                    <th>rg</th>
-                    <th>endereço</th>
-                    <th>numero</th>
-                    <th>estado</th>
-                    <th>cidade</th>
-                    <th>renda</th>
-                    <th>cadastrado por</th>
-                    <th>created</th>
-                    <th>status</th>
-                    <th>edit</th>
-                    <th>delete</th>
+                    <th scope='col'>Id</th>
+                    <th scope='col'>Nome</th>
+                    <th scope='col'>RG</th>
+                    <th scope='col'>Endereço</th>
+                    <th scope='col'>Número</th>
+                    <th scope='col'>Estado</th>
+                    <th scope='col'>Cidade</th>
+                    <th scope='col'>Renda</th>
+                    <th scope='col'>Cadastrado por</th>
+                    <th scope='col'>Created</th>
+                    <th scope='col'>Status</th>
+                    <th scope='col'>Editar</th>
+                    <th scope='col'>Deletar</th>
                 </tr>";
                 foreach ($clientes as $cliente) {
                     echo "<tr>";
                     foreach ($cliente as $key => $value) {
-                        echo "<td>".$value."</td>"; 
+                        if ($key != "cpf") {
+                            echo "<td>".$value."</td>"; 
+                        }
                     }
                     echo "<td>". anchor('clientes/editar/'.$cliente->id.'', 'Editar', 'title="editar" id="links"') ."</td>";
                     echo "<td>". anchor('clientes/deletar/'.$cliente->id.'', 'Deletar', 'title="deletar" id="links"')."</td>";
                     echo "</tr>";
                 }
             ?>
-            </table>
-            <?php echo anchor('clientes/adicionar', 'Cadastrar novo cliente', 'title="adicionar" id="links"') ?>
-            <?php echo anchor('usuarios', 'Retornar', 'title="usuarios" id="links"') ?>
-    
-        </div>
+        </table>
+        <?php echo anchor('clientes/adicionar', 'Cadastrar novo cliente', 'title="adicionar" ') . "<br>" ?>
+        <?php echo anchor('usuarios', 'Retornar', 'title="usuarios" id="links"') ?>
 
-    </body>
+    </div>
+
+</body>
 
 </html>
