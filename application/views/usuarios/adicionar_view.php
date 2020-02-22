15ea68867db8ef68bf0b46e8fdee62e1e4f89d6b
wildrimak <=>
2020-02-21 23:02:43 -0300

    Melhorado visualização do cadastro de usuarios

--- application/views/usuarios/adicionar_view.php
+++ application/views/usuarios/adicionar_view.php
@@ -1,55 +1,69 @@
 <!doctype html>
 <html lang="pt-br">
 
-    <head>
-        <title>Adicionar usuario</title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Login">
 
-    <body>
-        <?php echo anchor('usuarios', 'Retornar', 'title="usuarios" id="links"') ?>
-        <?php echo form_open('usuarios/salvar'); ?>
-            <div align="center">
-                <h2>Cadastre um novo usuário admin para o sistema!</h2>
+    <title>Adicionar novo usuário</title>
 
-                <div>
-                    <?php if( $this->session->flashdata('authError') )
-                        {
-                            echo $this->session->flashdata('authError');
-                        }
-                    ?>
-                </div>
+    <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.css');?>">
+</head>
 
-                <div>
-                    <h5>Nome</h5>
-                    <?php echo form_error('nome'); ?>
-                    <input type="text" name="nome" value="<?php echo set_value('nome'); ?>" />
-                </div>
 
-                <div>
-                    <h5>Matricula</h5>
-                    <?php echo form_error('matricula'); ?>
-                    <input type="text" name="matricula" value="<?php echo set_value('matricula'); ?>"/>    
-                </div>
+<body class="text-center">
 
-                <div>
-                    <h5>Senha</h5>
-                    <?php echo form_error('senha'); ?>
-                    <input type="password" name="senha" value="<?php echo set_value('senha'); ?>" />
-                </div>
+    <?php echo form_open('usuarios/salvar', 'class="form-singin col-md-12"'); ?>
+    <div class="container">
 
-                <div>
-                    <h5>Confirmar Senha</h5>
-                    <?php echo form_error('confirmar_senha'); ?>
-                    <input type="password" name="confirmar_senha" value="<?php echo set_value('confirmar_senha'); ?>" />
-                </div>
+        <div>
+            <h3 class="h3 mb-3 font-weight-normal"> Cadastre um novo usuário admin para o sistema!</h3>
+        </div>
 
-                <div>
-                    <input type="submit" value="Submit" />
-                </div>
 
+        <div>
+            <?php 
+                if( $this->session->flashdata('authError') )
+                    {
+                        echo $this->session->flashdata('authError');
+                    }
+                ?>
+        </div>
+
+        <div>
+            <div>
+                <?php echo form_error('nome'); ?>
+                <input type="text" placeholder="Escreva seu nome" name="nome" class="form-control" value="<?php echo set_value('nome'); ?>" />
             </div>
-        <?php echo form_close() ?>
 
-    </body>
+            <div>
+                <?php echo form_error('matricula'); ?>
+                <input type="text" placeholder="Digite uma matricula " class="form-control" name="matricula"
+                    value="<?php echo set_value('matricula'); ?>" />
+            </div>
+
+            <div>
+                <?php echo form_error('senha'); ?>
+                <input type="password" placeholder="Digite sua senha" class="form-control" name="senha" value="<?php echo set_value('senha'); ?>" />
+            </div>
+
+            <div>
+                <?php echo form_error('confirmar_senha'); ?>
+                <input type="password" placeholder="Confirme sua senha " class="form-control" name="confirmar_senha"
+                    value="<?php echo set_value('confirmar_senha'); ?>" />
+            </div>
+        </div>
+        <div>
+            <div>
+                <input type="submit" class="btn btn-lg btn-primary btn-block" value="Cadastrar" />
+            </div>
+        </div>
+
+    </div>
+    <?php echo form_close() ?>
+
+    <?php echo anchor('usuarios', 'Retornar', 'title="usuarios" id="links"') ?>
+</body>
 
 </html>
